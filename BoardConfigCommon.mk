5326448b0a4954ee5d6dc10f9cfdedbc7b223b82
xNUTx <nut@fun-industries.nl>
2015-01-09 22:32:31 +0100

    As there is no difference between TWRP graphics files, we'll use the one from Team Win for now.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -153,7 +153,7 @@ TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
 TW_CRYPTO_FS_FLAGS := "0x00000406"
 TW_CRYPTO_KEY_LOC := "footer"
 TW_INCLUDE_FUSE_EXFAT := true
-TW_BOARD_CUSTOM_GRAPHICS := ../../../device/sony/msm8226-common/recovery/twrpgraphics.c
+# TW_BOARD_CUSTOM_GRAPHICS := ../../../device/sony/msm8226-common/recovery/twrpgraphics.c
 TW_BRIGHTNESS_PATH := /sys/class/leds/lm3630-lcd-bl/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_NO_USB_STORAGE := true
